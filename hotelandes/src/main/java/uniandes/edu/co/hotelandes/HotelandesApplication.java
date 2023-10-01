7d9031c9f5e0ff19db5d8fe1c97387843787759c
Andres Carrasquilla <Andres>
2023-10-01 11:48:31 -0500

    ddd

--- hotelandes/src/main/java/uniandes/edu/co/hotelandes/HotelandesApplication.java
+++ hotelandes/src/main/java/uniandes/edu/co/hotelandes/HotelandesApplication.java
@@ -1,14 +1,6 @@
 package uniandes.edu.co.hotelandes;
-
-import java.util.Collection;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import uniandes.edu.co.hotelandes.modelo.Habitacion;
-import uniandes.edu.co.hotelandes.repositorio.HabitacionRepository;
 
 @SpringBootApplication
 public class HotelandesApplication{
@@ -16,5 +8,4 @@ public class HotelandesApplication{
 	public static void main(String[] args) {
 		SpringApplication.run(HotelandesApplication.class, args);
 	}
-
 }
