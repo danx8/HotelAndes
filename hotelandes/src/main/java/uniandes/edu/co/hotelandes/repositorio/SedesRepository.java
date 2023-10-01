e3c1ae3d714b019f60f060a0d14325200ed7a07f
Andres <j.carrasquillag@uniandes.edu.co>
2023-09-30 19:25:58 -0500

    Mas avances

---
+++ hotelandes/src/main/java/uniandes/edu/co/hotelandes/repositorio/SedesRepository.java
@@ -0,0 +1,34 @@
+package uniandes.edu.co.hotelandes.repositorio;
+
+
+import java.util.Collection;
+
+import org.springframework.data.jdbc.repository.query.Modifying;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import jakarta.transaction.Transactional;
+import uniandes.edu.co.hotelandes.modelo.Sede;
+
+public interface SedesRepository extends JpaRepository<Sede, Integer> {
+    
+    @Query(value = "SELECT * FROM Sedes", nativeQuery = true)
+    Collection<Sede> darSedes();
+
+    @Query(value = "SELECT FROM Sedes WHERE id= :id", nativeQuery = true)
+    Sede darSede(@Param("id") Integer id);
+
+    @Modifying
+    @Transactional
+    @Query(value = "INSERT INTO Sedes(id, nombre, telefono, direccion) VALUES(1, nombre= :nombre, telefono= :telefono, direccion= :direccion)", nativeQuery = true)
+    void updateSede(@Param("nombre") String nombre, @Param("telefono") String telefono, @Param("direccion") String direccion);
+
+    @Modifying
+    @Transactional
+    @Query(value = "DELETE FROM Sedes WHERE id= :id", nativeQuery = true)
+    void deleteSede(@Param("id") Integer id);
+
+
+    
+}
---
+++ hotelandes/src/main/java/uniandes/edu/co/hotelandes/repositorio/TipoHabitacionRepository.java
@@ -0,0 +1,34 @@
+package uniandes.edu.co.hotelandes.repositorio;
+
+
+import java.util.Collection;
+
+import org.springframework.data.jdbc.repository.query.Modifying;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
+
+import uniandes.edu.co.hotelandes.modelo.TipoHabitacion;
+
+public interface TipoHabitacionRepository extends JpaRepository<TipoHabitacion, Integer> {
+
+    @Query(value = "SELECT * FROM Tipos_habitacion", nativeQuery = true)
+    Collection<TipoHabitacion> darTiposHabitacion();
+
+    @Query(value = "SELECT FROM Tipos_habitacion WHERE id= :id", nativeQuery = true)
+    TipoHabitacion darTipoHabitacion(@Param("id") Integer id);
+
+    @Modifying
+    @Transactional
+    @Query(value = "UPDATE Tipos_habitacion SET id= :id, descripcion= :descripcion, nombre_tipo= :nombre_tipo", nativeQuery = true)
+    void updateTipoHabitacion(@Param("id") Integer id, @Param("descripcion") String descripcion, @Param("nombre_tipo") String nombre_tipo);
+
+    @Modifying
+    @Transactional
+    @Query(value = "DELETE FROM Tipo_habitacion WHERE id= :id", nativeQuery = true)
+    void deleteTipoHabiatcion(@Param("id") Integer id);
+
+
+    
+}
